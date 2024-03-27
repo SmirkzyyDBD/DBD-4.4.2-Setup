914acb84f8edebfe2b75fa3a4aa105a104e362ed
Smirkzyy <141465520+SmirkzyyDBD@users.noreply.github.com>
2024-03-27 17:54:24 +0000

    Update and rename url.py to download.json

--- 4.4.2 installer/url.py
+++
@@ -1,1 +0,0 @@
-fileURL = "https://cdn.discordapp.com/attachments/1184987629083705455/1216873281593020456/442_Launcher_Update_-_GrabSteamURL.zip?ex=6601f88e&is=65ef838e&hm=094371c004c4a5e0cfde59934c356e046e21299e45d288cd6762b26a1e119822&"
