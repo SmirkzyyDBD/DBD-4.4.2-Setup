99d3fedc3d3216809fc7273840af9ac24f114fbf
Smirkzyy <141465520+SmirkzyyDBD@users.noreply.github.com>
2024-03-27 22:58:41 +0000

    Add files via upload

--- 4.4.2 installer/main.py
+++ 4.4.2 installer/main.py
@@ -3,12 +3,13 @@ import requests
 from zipfile import ZipFile
 import urllib.parse
 import ctypes
-from url import fileURL
+from download import fileURL
+import hashlib
 
 def set_console_title(title):
     ctypes.windll.kernel32.SetConsoleTitleW(title)
 
-set_console_title("4.4.2 Launcher Installer v1.1")
+set_console_title("4.4.2 Launcher Installer v1.2")
 
 
 def download_and_extract(url, destination_folder):
@@ -34,21 +35,37 @@ def check_dbd_path(destination_folder):
 def get_path():
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
-        print("[MESSAGE] Enter the path to your 4.4.2 root folder:")
-        dbd_path = input("- ")
+        print("[MESSAGE] Enter the path to your DbD 4.4.2 root folder:")
+        dbd_path = input("[INPUT] - ")
 
-        if not os.path.exists(os.path.join(dbd_path, 'DeadByDaylight\\Binaries\\Win64\\steam_lobby.exe')):
-            print("[ERROR] 4.4.2 root folder was not found.")
+        dbd_exe_path = os.path.join(dbd_path, "DeadByDaylight.exe")
+        if not os.path.exists(dbd_exe_path):
+            print("[ERROR] DbD root folder was not found.")
             input("[MESSAGE] Press ENTER to continue...")
         else:
-            return dbd_path
+            expected_hash = "A071830466701D8FABEAC2DDF88A70DD"
+            md5_hash = compute_md5(dbd_exe_path)
+            if md5_hash.lower() != expected_hash.lower():
+                print("[ERROR] Wrong DbD version was entered.")
+                input("[MESSAGE] Press ENTER to continue...")
+            else:
+                return dbd_path
+        
+def compute_md5(file_path):
+    hash_md5 = hashlib.md5()
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096) , b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
 
 if __name__ == "__main__":
     download_url = fileURL
     
     DbD_path = get_path()
     
+    print("[MESSAGE] Installing files...")
     download_and_extract(download_url, DbD_path)
     
     print("[SUCCESS] Launcher installed succesfully.")
     input("[MESSAGE] Press ENTER to EXIT...")
+    
